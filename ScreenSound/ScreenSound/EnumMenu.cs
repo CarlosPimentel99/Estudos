6df1c30c0bb379690b4035a6ec9a858fd5aa28c4
Carlos Daniel Campos Pimentel <carlos.pimentel@LG003574.lggo.com.br>
2024-01-15 17:07:07 -0300

    criação do projeto screen sound

---
+++ ScreenSound/ScreenSound/EnumMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ScreenSound;
+
+public enum Menu
+{
+    [Description("Registrar uma Banda")]
+    RegistrarUmaBanda = 1,
+
+    [Description("Mostrar todas as Bandas")]
+    MostrarTodasAsBandas = 2,
+
+    [Description("Avaliar uma Banda")]
+    AvaliarUmaBanda = 3,
+
+    [Description("Exibir média de uma Banda")]
+    ExibirMediaDeUmaBanda = 4,
+
+    [Description("Sair")]
+    Sair = -1
+}
---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,63 @@
+﻿using ScreenSound;
+using System.ComponentModel;
+
+var _pularLinha = "\n";
+
+// execução do programa
+IniciarAplicacao();
+
+
+void IniciarAplicacao()
+{
+    Console.Write(@"
+░██████╗░█████╗░██████╗░███████╗███╗░░██╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝████╗░██║████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░██╔██╗██║██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██║╚████║██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗██║░╚███║██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚══╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.Write(_pularLinha);
+    Console.WriteLine("Seja bem vindo ao Screen Sound!!!!");
+    ExibirMenu();
+}
+
+void ExibirMenu()
+{
+    Console.Write(_pularLinha);
+    foreach (Menu opcao in Enum.GetValues(typeof(Menu)))
+    {
+        string descricao = ObterDescricaoDoMenu(opcao);
+        Console.WriteLine($"Digite {opcao.GetHashCode()} para {descricao}");
+    }
+
+    Console.Write(_pularLinha + "Digite a opção desejada: ");
+    var opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+    ValidarOpcaoDoMenuEscolhida(opcaoEscolhida);
+
+}
+
+string ObterDescricaoDoMenu(Menu opcao)
+{
+    var memberInfo = typeof(Menu).GetMember(opcao.ToString());
+    var descriptionAttribute = (DescriptionAttribute)Attribute.GetCustomAttribute(memberInfo[0], typeof(DescriptionAttribute));
+
+    return descriptionAttribute?.Description ?? opcao.ToString();
+}
+
+
+static void ValidarOpcaoDoMenuEscolhida(int opcaoEscolhida)
+{
+    if (opcaoEscolhida == Menu.Sair.GetHashCode())
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    } else if (opcaoEscolhida == Menu.RegistrarUmaBanda.GetHashCode())
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    }
+    else
+    {
+        Console.WriteLine("Nenhuma opção valida foi digitada!!!");
+    }
+
+}
