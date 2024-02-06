420e906b3095bb8a3d0f703f553cb7995a80ee99
Carlos Daniel Campos Pimentel <carlos.pimentel@LG003574.lggo.com.br>
2024-02-06 18:42:55 -0300

    Iniciando novo Projeto

---
+++ ScreenSoundV2/ScreenSoundV2/Program.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();
+
+musica1.Nome = "Wind of change";
+musica1.Artista = "Scorpions";
+musica1.Duracao = 265;
+musica1.Disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Ana Julia";
+musica2.Artista = "Los Hermanos";
+musica2.Duracao = 199;
+musica1.Disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
