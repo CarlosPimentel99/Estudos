feeed81941cd6796979d23df9c97ab92e1130518
Carlos Daniel Campos Pimentel <carlos.pimentel@LG003574.lggo.com.br>
2024-03-28 09:53:38 -0300

    Desenvolvimenro da API

---
+++ ASP.NET Core/FilmesAPI/FilmesAPI/Controllers/FilmesController.cs
@@ -0,0 +1,37 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
+
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Duracao);
+            Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public List<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme? RecuparFilmePorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
+        }
+    }
+}
---
+++ ASP.NET Core/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titulo do Filme é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Genero do Filme é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O tamanho máximo do gênero não pode exceder 50 catacteres")]
+        public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 à 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
